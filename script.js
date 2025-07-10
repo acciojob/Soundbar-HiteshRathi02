3fd5d846d0a3b0f4ff264347ca959410b563dee5
Hitesh Rathi <97667730+HiteshRathi02@users.noreply.github.com>
2025-07-10 08:32:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+const buttons = document.querySelectorAll(".btn");
+const stop = document.querySelector("#stop");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const text = button.innerText;
+    console.log(text)
+    const audio = new Audio(`sounds/${text}.mp3`);
+	  stop.click();
+    audio.play();
+  });
+});
+stop.addEventListener("click", () => {
+  const audios = document.querySelectorAll("audio");
+  audios.forEach((audio) => {
+    audio.pause();
+    audio.currentTime = 0; // Reset the audio to the beginning
+  });
+  console.log("All sounds stopped.");
+});
+
